b13318f01efa5a264f79a6946a380ebbcafbca4b
jkuya <kuyajesee@proton.me>
2025-04-24 00:59:11 +0300

    Add login functionality with dynamic form creation

---
+++ static/js/login.js
@@ -0,0 +1,45 @@
+export const login = () => {
+    let logincontainer = document.createElement("div");
+    logincontainer.classList.add("login-container");
+
+    let form = document.createElement("form");
+    form.classList.add("login-form");
+    let h1 = document.createElement("h1");
+    h1.innerText = "Login";
+
+    let name = document.createElement("div");
+    name.classList.add("input-group");
+    let nameLabel = document.createElement("label");
+    nameLabel.setAttribute("for", "username");
+    nameLabel.innerText = "Username/Email";
+    let nameInput = document.createElement("input");
+    nameInput.setAttribute("type", "text");
+    nameInput.setAttribute("name", "username");
+    nameInput.setAttribute("id", "username");
+    nameInput.required = true;
+    name.appendChild(nameLabel);
+    name.appendChild(nameInput);
+
+    let password = document.createElement("div");
+    password.classList.add("input-group");
+    let passwordLabel = document.createElement("label");
+    passwordLabel.setAttribute("for", "password");
+    passwordLabel.innerText = "Password";
+    let passwordInput = document.createElement("input");
+    passwordInput.setAttribute("type", "password");
+    passwordInput.setAttribute("name", "password");
+    passwordInput.setAttribute("id", "password");
+    passwordInput.required = true;
+    password.appendChild(passwordLabel);
+    password.appendChild(passwordInput);
+
+    let submit = document.createElement("button");
+    submit.setAttribute("type", "submit");
+    submit.innerText = "Login";
+
+    form.appendChild(h1);
+    form.appendChild(name);
+    form.appendChild(password);
+    form.appendChild(submit);
+    logincontainer.appendChild(form);
+}
