2d0b090677ae57d4047a27c435c3415004e63bea
jkuya <kuyajesee@proton.me>
2025-04-24 01:15:34 +0300

    feat: Add setHeader function to dynamically manage document head elements

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,21 @@ import { login } from './login.js';
 document.addEventListener('DOMContentLoaded', function() {
     login();
 });
+
+export const setHeader = (link) => {
+    document.head.innerHTML = "";
+    let title = document.createElement("title");
+    title.innerText = "GraphQL";
+    document.head.appendChild(title);
+    document.head.appendChild(link);
+    let script = document.createElement("script");
+    script.setAttribute("src", "static/js/script.js");
+    document.head.appendChild(script);
+    let meta = document.createElement("meta");
+    meta.setAttribute("name", "viewport");
+    meta.setAttribute("content", "width=device-width, initial-scale=1.0");
+    document.head.appendChild(meta);
+    let meta2 = document.createElement("meta");
+    meta2.setAttribute("charset", "utf-8");
+    document.head.appendChild(meta2);
+}
