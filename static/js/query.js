43b5fa4fe8ea48ae533a1c290b47266cbf308cff
jkuya <kuyajesee@proton.me>
2025-04-25 03:25:55 +0300

    feat: Refactor query.js to encapsulate GraphQL endpoint and query in a function

--- static/js/query.js
+++ static/js/query.js
@@ -1,18 +1,21 @@
-const endpoint = "https://learn.zone01kisumu.ke/api/graphql-engine/v1/graphql";
+const getData = () => {
+    const endpoint = "https://learn.zone01kisumu.ke/api/graphql-engine/v1/graphql";
 
-const query = `
-    {
-        user {
-            id
-        }
-}
-`
-
-
-fetch(endpoint, {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${sessionStorage.getItem("token")}`,
+    const query = `
+        {
+            user {
+                id
+            }
     }
-})
+    `
+
+
+    fetch(endpoint, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${sessionStorage.getItem("token")}`,
+        }
+    })
+
+}
