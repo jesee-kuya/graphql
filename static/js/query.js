4dd3d625f96a5353a286d527599d48157b648f22
jkuya <kuyajesee@proton.me>
2025-04-25 03:22:47 +0300

    feat: Add GraphQL user query and fetch implementation in query.js

--- static/js/query.js
+++ static/js/query.js
@@ -1,5 +1,17 @@
 const endpoint = "https://learn.zone01kisumu.ke/api/graphql-engine/v1/graphql";
 
 const query = `
-    
+    {
+        user {
+            id}
+}
 `
+
+
+fetch(endpoint, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${sessionStorage.getItem("token")}`,
+    }
+})
