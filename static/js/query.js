b2752486ddf107f1cee200573ce78d1369b6516a
jkuya <kuyajesee@proton.me>
2025-04-25 03:30:31 +0300

    feat: Update getData function to be asynchronous and handle errors during data fetching

--- static/js/query.js
+++ static/js/query.js
@@ -1,6 +1,6 @@
 import { profile } from "./profile.js";
 
-export const getData = () => {
+export const getData = async () => {
     const endpoint = "https://learn.zone01kisumu.ke/api/graphql-engine/v1/graphql";
 
     const query = `
@@ -11,13 +11,23 @@ export const getData = () => {
     }
     `
 
+    try {
+        let response = await fetch(endpoint, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${sessionStorage.getItem("token")}`,
+            },
+        })
 
-    fetch(endpoint, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${sessionStorage.getItem("token")}`,
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
         }
-    })
+        const data = await response.json();
+        console.log(data);
+        profile(data);
+    } catch (error) {
+        alert(`Error during sign-in: ${error}`);
+    }
 
 }
