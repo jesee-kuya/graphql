5ff24142a2dba12c46bac20e44b414c1cee4fb87
jkuya <kuyajesee@proton.me>
2025-04-29 01:17:52 +0300

    feat: Update GraphQL query to retrieve user details based on session user ID and include additional data fields

--- static/js/query.js
+++ static/js/query.js
@@ -3,15 +3,50 @@ import { profile } from "./profile.js";
 export const getData = async () => {
     const endpoint = "https://learn.zone01kisumu.ke/api/graphql-engine/v1/graphql";
 
+    let userId = sessionStorage.getItem("user_id");
+
     const query = `
-        {
-            user {
-                id
-                login
-                campus
-                attrs
+    {
+        user(where: {id: {_eq: "${userId}"}}) {
+          # User Details
+          id
+          profile
+          campus
+          
+          # Best Skills (Top 5 highest graded results)
+          results(order_by: {grade: desc}, limit: 5) {
+            object {
+              name
             }
-    }
+            grade
+          }
+          
+          # XP Information
+          transactions(where: {type: {_eq: "xp"}}) {
+            amount
+          }
+          
+          # Level Information (assuming stored in attrs)
+          attrs
+          
+          # Audit Ratio Components
+          upTransactions: transactions(where: {type: {_eq: "up"}}) {
+            amount
+          }
+          downTransactions: transactions(where: {type: {_eq: "down"}}) {
+            amount
+          }
+          
+          # XP Progression Timeline
+          xpTimeline: transactions(
+            where: {type: {_eq: "xp"}}
+            order_by: {createdAt: asc}
+          ) {
+            amount
+            createdAt
+          }
+        }
+      }
     `
 
     try {
